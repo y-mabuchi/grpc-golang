e4ac453e4cd509605d50e4ce5e8180ef310a6980
y-mabuchi <mabu.you@gmail.com>
2021-05-13 15:57:29 +0900

    done section6 part30

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/y-mabuchi/grpc-golang/calculator/calculatorpb"
@@ -22,6 +23,12 @@ func main() {
 	c := calculatorpb.NewCalculatorServiceClient(cc)
 	// fmt.Printf("Created client: %f", c)
 
+	// doSum(c)
+
+	doPrime(c)
+}
+
+func doSum(c calculatorpb.CalculatorServiceClient) {
 	req := &calculatorpb.SumRequest{
 		FirstNumber:  3,
 		SecondNumber: 10,
@@ -32,4 +39,26 @@ func main() {
 		log.Fatalf("error while calling Sum RPC: %v", err)
 	}
 	log.Printf("Response from Sum: %v\n", res.Result)
+}
+
+func doPrime(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a PrimeNumberDecomposition Server Streaming RPC...")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		InputNumber: 120,
+	}
+
+	res, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling PrimeNumberDecomposition: %v", err)
+	}
+	for {
+		msg, err := res.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream: %v", err)
+		}
+		log.Printf("Response from PrimeNumberDecomposition: %v", msg.GetResult())
+	}
 }
--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -20,6 +20,27 @@ func (*server) Sum(ctx context.Context, in *calculatorpb.SumRequest) (*calculato
 	return res, nil
 }
 
+func (*server) PrimeNumberDecomposition(
+	req *calculatorpb.PrimeNumberDecompositionRequest,
+	stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("PrimeNumberDecomposition function was invoked with %v\n", req)
+	divisor := int32(2)
+	number := req.GetInputNumber()
+	for number > 1 {
+		if number%divisor == 0 {
+			res := &calculatorpb.PrimeNumberDecompositionResponse{
+				Result: divisor,
+			}
+			stream.Send(res)
+			number = number / divisor
+		} else {
+			divisor++
+			fmt.Printf("Divisor has increased to %v\n", divisor)
+		}
+	}
+	return nil
+}
+
 func main() {
 	fmt.Println("Hello world")
 
